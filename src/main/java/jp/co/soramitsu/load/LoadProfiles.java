a8ea0809d8446d4cc23c73d7e392fc0cc80396cb
timofeevmd <timofeevmd@gmail.com>
2023-10-10 14:19:12 +0400

    update load model
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- src/main/java/jp/co/soramitsu/load/LoadProfiles.java
+++ src/main/java/jp/co/soramitsu/load/LoadProfiles.java
@@ -1,11 +1,28 @@
 package jp.co.soramitsu.load;
 
+import io.gatling.javaapi.core.ClosedInjectionStep;
 import io.gatling.javaapi.core.OpenInjectionStep;
 import jp.co.soramitsu.load.infrastructure.config.SimulationConfig;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 
 public class LoadProfiles {
+
+    public static ClosedInjectionStep getMaxPerformanceClosedProfileOld() {
+        return incrementConcurrentUsers(1)
+                .times(200)
+                .eachLevelLasting(30)
+                .separatedByRampsLasting(1)
+                .startingFrom(0);
+    }
+    public static ClosedInjectionStep getMaxPerformanceClosedProfile() {
+        return incrementConcurrentUsers(SimulationConfig.simulation.concurrentUsers())
+                .times(SimulationConfig.simulation.times())
+                .eachLevelLasting(SimulationConfig.simulation.eachLevelLasting())
+                .separatedByRampsLasting(SimulationConfig.simulation.separatedByRampsLasting())
+                .startingFrom(SimulationConfig.simulation.startingFrom());
+    }
+
     public static OpenInjectionStep[] setupModel(){
         return new OpenInjectionStep[]{
                 rampUsers(SimulationConfig.simulation.domainSetUpRumpUp()).during(SimulationConfig.simulation.duringSetUp())
