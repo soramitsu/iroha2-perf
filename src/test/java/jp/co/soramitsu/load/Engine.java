93fa06694426023a9d8b3bdbb69a99b3f12f7d0f
timofeevmd <timofeevmd@gmail.com>
2023-11-20 11:24:02 +0400

    added healthcheck
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- src/test/java/jp/co/soramitsu/load/Engine.java
+++ src/test/java/jp/co/soramitsu/load/Engine.java
@@ -2,15 +2,17 @@ package jp.co.soramitsu.load;
 
 import io.gatling.app.Gatling;
 import io.gatling.core.config.GatlingPropertiesBuilder;
+import io.micronaut.runtime.Micronaut;
 import jp.co.soramitsu.load.infrastructure.config.SimulationConfig;
 
-public class  Engine {
+public class Engine {
     public static void main(String[] args) {
-            GatlingPropertiesBuilder props = new GatlingPropertiesBuilder()
-                    .resourcesDirectory(IDEPathHelper.mavenResourcesDirectory.toString())
-                    .resultsDirectory(IDEPathHelper.resultsDirectory.toString())
-                    .binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString())
-                    .simulationClass(SimulationConfig.simulation.maximumSearchSimulation());
-            Gatling.fromMap(props.build());
+        Micronaut.run(Engine.class);
+        GatlingPropertiesBuilder props = new GatlingPropertiesBuilder()
+                .resourcesDirectory(IDEPathHelper.mavenResourcesDirectory.toString())
+                .resultsDirectory(IDEPathHelper.resultsDirectory.toString())
+                .binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString())
+                .simulationClass(SimulationConfig.simulation.maximumSearchSimulation());
+        Gatling.fromMap(props.build());
     }
 }
