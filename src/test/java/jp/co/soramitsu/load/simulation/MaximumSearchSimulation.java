26a0ae2a27674a333781ab141ea98b0b3d60750d
timofeevmd <timofeevmd@gmail.com>
2023-11-27 19:24:35 +0400

    docker-compose.yml updated
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- src/test/java/jp/co/soramitsu/load/simulation/MaximumSearchSimulation.java
+++ src/test/java/jp/co/soramitsu/load/simulation/MaximumSearchSimulation.java
@@ -4,15 +4,18 @@ import io.gatling.javaapi.core.OpenInjectionStep;
 import io.gatling.javaapi.core.Simulation;
 import jp.co.soramitsu.load.*;
 
+import java.time.Duration;
+
 import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
+import static io.gatling.javaapi.core.OpenInjectionStep.nothingFor;
 
 public class MaximumSearchSimulation extends Simulation {
     {
         setUp(
                 jp.co.soramitsu.load.SetUp.Companion.apply().injectOpen(OpenInjectionStep.atOnceUsers(1))
-                        .andThen(Iroha2SetUp.Companion.apply().injectOpen(LoadProfiles.setupModel())
-                                .andThen(TransactionOnly.Companion.apply().injectClosed(LoadProfiles.getMaxPerformanceClosedProfile()))
-                        ).andThen(CleanUp.Companion.apply().injectOpen(OpenInjectionStep.atOnceUsers(1)))
+                        .andThen(Iroha2SetUp.Companion.apply().injectOpen(LoadProfiles.setupModel()))
+                        .andThen(TransactionOnly.Companion.apply().injectClosed(LoadProfiles.getMaxPerformanceClosedProfile()))
+                        .andThen(CleanUp.Companion.apply().injectOpen(OpenInjectionStep.atOnceUsers(1)))
         ).protocols(Protocols.httpProtocol);
     }
 }
