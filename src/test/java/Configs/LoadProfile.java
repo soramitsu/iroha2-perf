ce05c72872fe4e4581f97bc641fcebf2c10593e5
timofeevmd <timofeevmd@gmail.com>
2024-07-02 21:10:22 +0400

    update scns
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- src/test/java/Configs/LoadProfile.java
+++ src/test/java/Configs/LoadProfile.java
@@ -7,11 +7,11 @@ import static io.gatling.javaapi.core.CoreDsl.rampUsers;
 
 public class LoadProfile {
     public static OpenInjectionStep getMaxPerformanceOpenProfile() {
-        return incrementUsersPerSec(Double.parseDouble(System.getProperty("concurrentUsers")))
-                .times(Integer.parseInt(System.getProperty("times")))
-                .eachLevelLasting(Integer.parseInt(System.getProperty("stageDuration")))
-                .separatedByRampsLasting(Integer.parseInt(System.getProperty("rampDuration")))
-                .startingFrom(Integer.parseInt(System.getProperty("startingFrom")));
+        return incrementUsersPerSec(Double.parseDouble(System.getProperty("concurrentUsers")))//20
+                .times(Integer.parseInt(System.getProperty("times")))//5
+                .eachLevelLasting(Integer.parseInt(System.getProperty("stageDuration")))//5
+                .separatedByRampsLasting(Integer.parseInt(System.getProperty("rampDuration")))//5
+                .startingFrom(Integer.parseInt(System.getProperty("startingFrom")));//20
     }
 
     public static OpenInjectionStep getMaxPerformance() {
--- src/test/java/simulation/PerformanceSimulation.java
+++ src/test/java/simulation/PerformanceSimulation.java
@@ -5,9 +5,10 @@ import io.gatling.javaapi.core.Simulation;
 
 import static Configs.Protocol.httpProtocol;
 import static scenarious.GeneralQueries.generalQueries;
+import static scenarious.HealthCheck.healthCheck;
 
 public class PerformanceSimulation extends Simulation {
     {
-        setUp(generalQueries.injectOpen(LoadProfile.getMaxPerformance()).protocols(httpProtocol));
+        setUp(healthCheck.injectOpen(LoadProfile.getMaxPerformance()).protocols(httpProtocol)).maxDuration(Long.parseLong(System.getProperty("maxDuration")));
     }
 }
