03cd43a41b4c2a900eafc96a2691602cbdd32c88
timofeevmd <timofeevmd@gmail.com>
2024-07-08 18:39:57 +0400

    update genesis.json, added new scn, update previous scn
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- src/test/java/requests/Transactions.java
+++ src/test/java/requests/Transactions.java
@@ -1,16 +1,41 @@
 package requests;
 
 import io.gatling.javaapi.core.ChainBuilder;
+import jp.co.soramitsu.iroha2.CryptoUtils;
 import jp.co.soramitsu.iroha2.ExtensionsKt;
-import jp.co.soramitsu.iroha2.generated.SignedTransaction;
+import jp.co.soramitsu.iroha2.generated.*;
 import jp.co.soramitsu.iroha2.transaction.TransactionBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 
-
 public class Transactions extends Constants {
+
+    public static ChainBuilder postRegisterDefinitionId = exec(feed(CSV_FEEDER)).exec(feed(PEERS_FEEDER))
+            .exec(
+                    http("tx_register_definition_id")
+                            .post(session -> {
+                                        return session.getString("peer") + Constants.URL_TRANSACTION;
+                                    }
+                            )
+                            .body(ByteArrayBody(session -> {
+                                                return SignedTransaction.Companion.encode(
+                                                        TransactionBuilder.Companion.builder()
+                                                                .account(ExtensionsKt.asAccountId(session.getString("anotherDevAccountIdSender")))
+                                                                .chainId(Constants.CHAIN_ID)
+                                                                .registerAssetDefinition(
+                                                                        ExtensionsKt.asAssetDefinitionId(session.getString("domainIdSender")),
+                                                                        new AssetType.Numeric(new NumericSpec())
+                                                                )
+                                                                .buildSigned(CryptoUtils.keyPairFromHex(
+                                                                        session.getString("publicKeySender"),
+                                                                        session.getString("privateKeySender")))
+                                                );
+                                            }
+                                    )
+                            )
+            ).exec(http("tx_register_definition_id_status").get(Constants.URL_STATUS).check(status().is(200)));
 
     public static ChainBuilder postRegisterDomain = exec(feed(CSV_FEEDER)).exec(feed(PEERS_FEEDER))
             .exec(
@@ -25,11 +50,14 @@ public class Transactions extends Constants {
                                                                 .account(ExtensionsKt.asAccountId(session.getString("anotherDevAccountIdSender")))
                                                                 .chainId(Constants.CHAIN_ID)
                                                                 .registerDomain(Constants.NEW_DOMAIN_ID)
-                                                                .buildSigned(Constants.ALICE_KEYPAIR));
+                                                                .buildSigned(CryptoUtils.keyPairFromHex(
+                                                                        session.getString("publicKeySender"),
+                                                                        session.getString("privateKeySender")))
+                                                );
                                             }
                                     )
                             )
-            ).exec(http("tx_register_domain_status").get(Constants.URL_STATUS).check(status().is(200)));;
+            ).exec(http("tx_register_domain_status").get(Constants.URL_STATUS).check(status().is(200)));
 
     public static ChainBuilder postTransferAsset = exec(feed(CSV_FEEDER)).exec(feed(PEERS_FEEDER))
             .exec(
@@ -46,7 +74,11 @@ public class Transactions extends Constants {
                                                                         1,
                                                                         ExtensionsKt.asAccountId(session.getString("anotherDevAccountIdReceiver"))
                                                                 )
-                                                                .buildSigned(Constants.ALICE_KEYPAIR));
+                                                                .buildSigned(CryptoUtils.keyPairFromHex(
+                                                                        session.getString("publicKeySender"),
+                                                                        session.getString("privateKeySender")
+                                                                ))
+                                                );
                                             }
                                     )
                             )
