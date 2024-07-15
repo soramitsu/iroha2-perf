24c8567084e611ee71ea9d03090fb228d2b8bf0e
timofeevmd <timofeevmd@gmail.com>
2024-07-15 11:58:35 +0400

    Merge remote-tracking branch 'origin/update/genesis' into update/genesis
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- src/test/java/requests/Transactions.java
+++ src/test/java/requests/Transactions.java
@@ -3,16 +3,12 @@ package requests;
 import io.gatling.javaapi.core.ChainBuilder;
 import jp.co.soramitsu.iroha2.CryptoUtils;
 import jp.co.soramitsu.iroha2.ExtensionsKt;
-import jp.co.soramitsu.iroha2.generated.AssetType;
-import jp.co.soramitsu.iroha2.generated.NumericSpec;
-import jp.co.soramitsu.iroha2.generated.SignedTransaction;
+import jp.co.soramitsu.iroha2.generated.*;
 import jp.co.soramitsu.iroha2.transaction.TransactionBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
-import static requests.Constants.CSV_FEEDER;
-import static requests.Constants.PEERS_FEEDER;
 
 public class Transactions extends Constants {
 
@@ -119,7 +115,7 @@ public class Transactions extends Constants {
                             ).body(ByteArrayBody(session -> {
                                                 return SignedTransaction.Companion.encode(
                                                         TransactionBuilder.Companion.builder()
-                                                                .account(ExtensionsKt.asAccountId("b24ff6ab1e5923f125d7f4eb0c62528eb6b8e84139e17fb377e2bd5fc1498b19@bulb_7405035c-e673-4a46-8f7f-1cca0a0687fa_6138d709-b7e0-44e6-b5dd-1f26f848f464"))
+                                                                /*crypto*/.account(ExtensionsKt.asAccountId("b24ff6ab1e5923f125d7f4eb0c62528eb6b8e84139e17fb377e2bd5fc1498b19@bulb_7405035c-e673-4a46-8f7f-1cca0a0687fa_6138d709-b7e0-44e6-b5dd-1f26f848f464"))
                                                                 .chainId(Constants.CHAIN_ID)
                                                                 /* drop ed0120*/.transferAsset(ExtensionsKt.asAssetId("xora08da796-bca3-475f-bf94-30cc918ef69b_c687b817-3a68-4848-81bc-d4915801cee1##b24ff6ab1e5923f125d7f4eb0c62528eb6b8e84139e17fb377e2bd5fc1498b19@bulb_7405035c-e673-4a46-8f7f-1cca0a0687fa_6138d709-b7e0-44e6-b5dd-1f26f848f464"),
                                                                         1,
@@ -130,6 +126,16 @@ public class Transactions extends Constants {
                                             );
                                             }
                                     )
+                            ).check(
+                                    status().is(200),
+                                    bodyString().saveAs("responseBody")
                             )
-            ).exec(http("debugging_tx_register_domain_status").get(Constants.URL_STATUS).check(status().is(200)));
+            ).exec(http("debugging_tx_register_domain_status").get(Constants.URL_STATUS).check(status().is(200)))
+            .exec(session -> {
+                String responseBody = session.getString("responseBody");
+                System.out.println("Response: " + responseBody);
+                return session;
+            });
 }
+
+
