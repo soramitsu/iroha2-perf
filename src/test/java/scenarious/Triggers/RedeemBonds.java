2467b49e2625318b20d2fb470b7929ec8c06f9b7
timofeevmd <timofeevmd@gmail.com>
2024-10-07 19:02:48 +0400

    updated pom.xml io.netty Signed-off-by: timofeevmd <timofeevmd@gmail.com>
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- src/test/java/scenarious/Triggers/RedeemBonds.java
+++ src/test/java/scenarious/Triggers/RedeemBonds.java
@@ -9,7 +9,7 @@ import static io.gatling.javaapi.core.CoreDsl.csv;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 
 public class RedeemBonds {
-    public static ScenarioBuilder redeemBonds = scenario("buy bonds")
+    public static ScenarioBuilder redeemBonds = scenario("redeem bonds")
             .feed(csv("preconditionList.csv").circular())
             .exec(Transactions.grantPermissionForTriggers)
             .pace(Duration.ofSeconds(10))
