dd2a7e7f73df9fd4c616d83074288455f4a7c7bc
timofeevmd <timofeevmd@gmail.com>
2024-10-14 14:02:26 +0400

    update load model Signed-off-by: timofeevmd <timofeevmd@gmail.com>
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- src/test/java/simulation/smartcontracts/standard/SmartContractsRegisterSimulation.java
+++ src/test/java/simulation/smartcontracts/standard/SmartContractsRegisterSimulation.java
@@ -1,20 +1,22 @@
 package simulation.smartcontracts.standard;
 
+import configs.gatling.LoadProfile;
 import io.gatling.javaapi.core.Simulation;
 
 import static configs.gatling.Protocol.httpProtocol;
 import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
+import static scenarious.triggers.BuyBonds.buyBonds;
+import static scenarious.triggers.RedeemBonds.redeemBonds;
 import static scenarious.triggers.RegisterSmartContracts.registerBondAsset;
+import static scenarious.triggers.RegisterSmartContracts.registerSmartContracts;
 
 public class SmartContractsRegisterSimulation extends Simulation{
     {
         setUp(
-                /*registerSmartContracts.injectOpen(atOnceUsers(1))
-                        //несколько бондов на 1 доме. колличество бондов в каждом домене может быть равным. создай не менее 5 на каждый домен
-                        .andThen(*/registerBondAsset.injectOpen(atOnceUsers(45))/*)*/ // 5 bonds on each domain
-                        //
-                        /*.andThen(buyBonds.injectOpen(LoadProfile.getMaxPerformanceOpenProfile()))
-                        .andThen(redeemBonds.injectOpen(atOnceUsers(1)))*/
+                registerSmartContracts.injectOpen(atOnceUsers(1))
+                        .andThen(registerBondAsset.injectOpen(atOnceUsers(45))) // 5 bonds on each domain
+                        .andThen(buyBonds.injectOpen(LoadProfile.getMaxPerformanceOpenProfile()))
+                        .andThen(redeemBonds.injectOpen(atOnceUsers(1)))
         ).protocols(httpProtocol);
     }
 }
