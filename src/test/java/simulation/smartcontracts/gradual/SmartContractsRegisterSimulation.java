8544cf979697f2b74d184cf81be2e4186103a954
timofeevmd <timofeevmd@gmail.com>
2024-10-14 15:21:43 +0400

    update load model Signed-off-by: timofeevmd <timofeevmd@gmail.com>
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

---
+++ src/test/java/simulation/smartcontracts/gradual/SmartContractsRegisterSimulation.java
@@ -0,0 +1,22 @@
+package simulation.smartcontracts.gradual;
+
+import configs.gatling.LoadProfile;
+import io.gatling.javaapi.core.Simulation;
+
+import static configs.gatling.Protocol.httpProtocol;
+import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
+import static scenarious.triggers.BuyBonds.buyBonds;
+import static scenarious.triggers.RedeemBonds.redeemBonds;
+import static scenarious.triggers.RegisterSmartContracts.registerBondAsset;
+import static scenarious.triggers.RegisterSmartContracts.registerSmartContracts;
+
+public class SmartContractsRegisterSimulation extends Simulation{
+    {
+        setUp(
+                registerSmartContracts.injectOpen(atOnceUsers(1))
+                        .andThen(registerBondAsset.injectOpen(atOnceUsers(45))) // 5 bonds on each domain
+                        .andThen(buyBonds.injectOpen(LoadProfile.getMaxPerformanceGradualLoadOpenProfile()))
+                        .andThen(redeemBonds.injectOpen(atOnceUsers(1)))
+        ).protocols(httpProtocol);
+    }
+}
