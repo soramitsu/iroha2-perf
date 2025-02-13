84a5561fb844520cbb60dce456807fc536b65aff
timofeevmd <timofeevmd@gmail.com>
2025-02-13 14:16:58 +0100

    genesis.json Signed-off-by: timofeevmd <timofeevmd@gmail.com>
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- performance-generator/src/test/java/requests/Transactions.java
+++ performance-generator/src/test/java/requests/Transactions.java
@@ -375,7 +375,10 @@ public class Transactions extends Constants {
                                                                 .transferAsset(ExtensionsKt.asAssetId(session.getString("anotherDevAssetIdSender")),
                                                                         1,
                                                                         ExtensionsKt.asAccountId(session.getString("anotherDevAccountIdReceiver")))
-                                                                .buildSigned(ALICE_KEYPAIR)
+                                                                .buildSigned(CryptoUtils.keyPairFromHex(
+                                                                        session.getString("publicKeySender"),
+                                                                        session.getString("privateKeySender")
+                                                                ))
                                                 );
                                             }
                                     )
